7407e258329ff54db32ca64b5a2fffa385c84687
sawsanders <74436859+sawsanders@users.noreply.github.com>
2026-01-03 12:45:22 -0800

    Update NTP accuracy and sync interval constants
    Signed-off-by: sawsanders <74436859+sawsanders@users.noreply.github.com>

--- src/ntp/client.c
+++ src/ntp/client.c
@@ -47,8 +47,8 @@
 // Required accuracy of the NTP sync in seconds in order to start the NTP server
 // thread. If the NTP sync is less accurate than this value, the NTP server
 // thread will only be started after later NTP syncs have reached this accuracy.
-// Default: 0.5 (seconds) **Changed to 1.5**
-#define ACCURACY 1.5
+// Default: 0.5 (seconds) **Changed to 300**
+#define ACCURACY 300
 
 // Interval between successive NTP sync attempts in seconds in case of
 // not-yet-sufficient accuracy of the NTP sync
